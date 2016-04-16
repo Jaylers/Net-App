2f10c346f09c14cdedd37c1e048f4ae62d88b3fa
Sapthawee Srichomthong <Sapthawee_S@cmu.ac.th>
2016-04-16 21:02:17 +0700

    Jaylerr update
    เพิ่มไฟล์ emmbed ที่เป้นไฟล์ css และอัพเดทการ comment ในแต่ละ review

--- reviewblog.php
+++ reviewblog.php
@@ -1,6 +1,7 @@
 <?php 
-	$rid = $_GET['reid'];
-	echo "review : ".$rid;
+	if(!empty($_GET['reid'])){
+		$rid = $_GET['reid'];
+	}
 	session_start();
 	if(!empty($_SESSION["ID"])){
 		$username=$_SESSION["Nickname"];
@@ -9,14 +10,48 @@
 		$username="unknow";
 	}
 
+    if(isset($_POST['detail']) and isset($_POST['agree']) and isset($_POST['rid']))
+    {
+       	$commentof = "unknow";
+       	$rid= $_POST['rid'];
+        $conndb = new PDO('mysql:host=localhost;dbname=moviesreview;charset=utf8','root','');
+		$sth = $conndb->prepare("SELECT reviewname FROM review WHERE reviewid = '$rid ");
+		$sth->execute();
+		if ($sth !== false) {
+		    while($row = $sth->fetch()) {
+		    	$commentof = $row['reviewname'];
+		    }
+		}
+		$conndb = null;
 
+
+	    if($_POST['agree']!=1){
+	    	$agree= 0;
+	    }else $agree= 1;
+	    $detail= $_POST['detail'];
+	    $owner= $username;
+
+        $servername ="localhost";
+        $username ="root";
+        $password ="";
+        $dbname ="moviesreview";
+        $conn = new mysqli($servername,$username,$password,$dbname);
+
+        if($conn->connect_error){
+        	die("Connection failed: ".$conn->connect_error);
+        }
+        $sql = "INSERT INTO Customer(detail,agree,owner,commentof) VALUES ('$detail','$agree','$owner','$commentof')";
+
+        $conn->query($sql);
+        $conn->close();
+    }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>  
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link href="embed.css" rel="stylesheet" >
+<link href="bootstrap/css/embed.css" rel="stylesheet" >
 <meta charset="utf-8">
 <style>
   body {background-color:lightgrey;}
@@ -88,7 +123,9 @@ body,td,th {
     ?>
     <ol class="breadcrumb"><br>
   	<li>
-    	<?php  
+
+    	<?php
+
         $conndb = new PDO('mysql:host=localhost;dbname=moviesreview;charset=utf8','root','');
 		$sth = $conndb->prepare("SELECT * FROM review where reviewid = $rid ");
 		$sth->execute();
@@ -98,12 +135,30 @@ body,td,th {
   				<li>
   				<?php
 		  		echo "<p align='center'><img src='".$row['image']."'style='width:500;height:300px;border:0;''><p>
-		  			<p align='left'><font size='6'>เรื่อง : ".$row['reviewname']."</fort> <font size='3'> review poit : ".$row['rate']."</fort> </p>
+		  		<p align='left'><font size='6'>เรื่อง : ".$row['reviewname']."</fort> <font size='3'> review poit : ".$row['rate'].
+		  		"</fort> </p>
 		  			<p align='left'><font size='4'>".$row['detail']."</fort></p>
-		  			<p align='left'><font size='2'>".$row['datetime']."</fort> <font size='2' color='blue'>".$row['owner']."</fort>  </p>";
+		  			<p align='left'><font size='2'>".$row['datetime']."</fort> <font size='2' color='blue'>".$row['owner'].
+		  			"</fort>  </p>";
 		  			?>
 		  		</li>
-		  		<?php echo "</ol>";
+		<?php  
+        $conndb = new PDO('mysql:host=localhost;dbname=moviesreview;charset=utf8','root','');
+		$sth = $conndb->prepare("SELECT agree FROM COMMENT");
+		$numofagree = 0;
+		$numofdisagree = 0;
+		$sth->execute();
+		if ($sth !== false) {
+		    while($row = $sth->fetch()) {
+		    	if($row['agree']==1){
+		    		$numofagree=$numofagree+1;
+		    	}else{$numofdisagree = $numofdisagree+1;}
+		    }
+		}
+		$conndb = null;
+		echo $numofagree." Person agree and ".$numofdisagree." Person disagree";
+        ?>
+		<?php echo "</ol>";
 		    }
 		}
 		$conndb = null; ?>
@@ -119,9 +174,9 @@ body,td,th {
   				<pre>
   				<?php
 		  		echo "<span class='displaytext'>
-		  			<font size='3'>เรื่อง : ".$row['commentof']."</fort>
-		  			<font size='4'>".$row['detail']."</fort>
-		  			<font size='2'>".$row['datetime']."</fort> <font size='2' color='blue'>".$row['owner']."</fort> </span>";
+		  			<font size='3'>".$row['detail']."</fort>
+		  			<font size='2'>".$row['datetime']."
+		  			</fort> <font size='2' color='blue'>".$row['owner']."</fort> </span>";
 		  			?>
 		  		</pre>
 		  		<?php echo "</ol>";
@@ -130,14 +185,20 @@ body,td,th {
 		$conndb = null;
         ?>
 
+<?php if( $username != "unknow") { ?>
 
 <ol class="breadcrumb"><br>
 <li>
-new comment
+	<form method="post" action="reviewblog.php">
+		<p> Comment <br/><textarea name="detail" id="detail" cols="40" rows="3"></textarea>
+		<br/><input type="checkbox" name="agree" value="1" /> Agree
+		<?php $rid; ?>
+		<br/><br/><input type="submit" name="mysubmit" id="mysubmit" value="Submit"/> </p> 
+
+	</form>
 </li>
+<?php } ?>
 
 <p align="center"><img src="image/cover.jpg"/></p>
 </body>
 </html>
-
-	
